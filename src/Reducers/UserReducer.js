1f27dfc46efd3998b90ca6191de0daf3da8355a2
Nnamdy1990 <Nnamdy1990@users.noreply.github.com>
2020-05-03 22:53:36 +0100

    new change

--- src/Reducers/UserReducer.js
+++ src/Reducers/UserReducer.js
@@ -1,4 +1,10 @@
-import { groupData, users, dummyMessages, data, groupData2 } from "../Data/Data";
+import {
+  groupData,
+  users,
+  dummyMessages,
+  data,
+  groupData2,
+} from "../Data/Data";
 
 const initialState = {
   groupData,
@@ -9,32 +15,33 @@ const initialState = {
   data,
   selectedGroup: null,
   newMessage: [],
-  valueReset: ""
-
+  valueReset: "",
 };
 
 const UserReducer = (state = initialState, action) => {
   const newstate = { ...state };
-  
+
   if (action.type === "GET_MESSAGE") {
     newstate.userTypedMessage = action.message;
   }
 
   if (action.type === "POST_MESSAGE") {
-    let postData = [{
-      memberName: "Simon Okah",
-      memberAvatar: "this is fourth message",
-      message: action.message,
-    }];
+    let postData = [
+      {
+        memberName: "Simon Okah",
+        memberAvatar: "this is fourth message",
+        message: action.message,
+      },
+    ];
     let newData = newstate.newMessage.concat(postData);
-    newstate.newMessage = newData
-    newstate.userTypedMessage = ""
+    newstate.newMessage = newData;
+    newstate.userTypedMessage = "";
   }
 
-  if (action.type === "GET_GROUP_NAME"){
-    console.log("Action NAme", action.groupName)
+  if (action.type === "GET_GROUP_NAME") {
+    console.log("Action NAme", action.groupName);
   }
-  
+
   return newstate;
 };
 
